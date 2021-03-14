0098719416e9fe433ea5dfd03883fe8b966b3b89
Ron Gerlach <wordsmith4u@hotmail.com>
2021-03-13 20:15:23 -0500

    MegaByteConverter

---
+++ MegaBytesConverter/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+
+        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+
+        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+
+    }
+}
---
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -0,0 +1,16 @@
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megabytes = (kiloBytes / 1024);
+            int kiloRemainder = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + kiloRemainder + " KB");
+        }
+    }
+
+
+
+}
